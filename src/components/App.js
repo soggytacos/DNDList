1f1aad72582f8eba625d0214f1d1ef81e798ebc0
Colman <colman.swisher@gmail.com>
2020-05-05 22:47:55 -0500

    added react-beautiful-dnd and some basic, non-persisting functionality to the cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,38 @@
 import React, {Component} from 'react';
 import TrelloList from "./TrelloList";
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import TrelloActionButton from "./TrelloActionButton";
+import {DragDropContext} from 'react-beautiful-dnd';
 
 
 class App extends Component {
-  render() {
 
-      const { lists } = this.props;
-    return (
-        <div className="App">
-          <h2>Hello World!</h2>
-            <div style={styles.listsContainer}>
-            { lists.map(list => (
-                <TrelloList
-                    listID={list.id}
-                    key={list.id}
-                    title={list.title}
-                    cards={list.cards}
-                />
-                ))}
-                <TrelloActionButton list />
-            </div>
-        </div>
-    );
-  }
+    onDragEnd = () => {
+        //reordering Logic
+    }
+
+    render() {
+        const {lists} = this.props;
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <div className="App">
+                    <h2>Hello World!</h2>
+                    <div style={styles.listsContainer}>
+                        {lists.map(list => (
+                            <TrelloList
+                                listID={list.id}
+                                key={list.id}
+                                title={list.title}
+                                cards={list.cards}
+                            />
+                        ))}
+                        <TrelloActionButton list/>
+                    </div>
+                </div>
+            </DragDropContext>
+        )
+            ;
+    }
 }
 
 const styles = {
--- src/reducers/listsReducer.js
+++ src/reducers/listsReducer.js
@@ -1,41 +1,41 @@
 import {CONSTANTS} from "../actions";
 
 let listID = 2;
-let cardID = 5;
+let cardID = 6;
 
 const initialState = [
     {
         title: "Last Episode",
-        id: 0,
+        id: `list-${0}`,
         cards: [
             {
-                id: 0,
+                id: `card-${0}`,
                 text: "we created a static list and a static card"
             },
             {
-                id: 1,
+                id: `card-${1}`,
                 text: "we used a mix between material UI React and styled components"
             }
         ]
     },
     {
         title: "This Episode",
-        id: 1,
+        id: `list-${1}`,
         cards: [
             {
-                id: 0,
+                id: `card-${2}`,
                 text: "we created a static list and a static card"
             },
             {
-                id: 1,
+                id: `card-${3}`,
                 text: "we used a mix between material UI React and styled components"
             },
             {
-                id: 2,
+                id: `card-${4}`,
                 text: "we created a static list and a static card"
             },
             {
-                id: 3,
+                id: `card-${5}`,
                 text: "we used a mix between material UI React and styled components"
             }
         ]
@@ -49,14 +49,14 @@ const listsReducer = (state = initialState, action) => {
             const newList = {
                 title: action.payload,
                 cards: [],
-                id: listID
+                id: `list-${listID}`
             };
             listID += 1;
             return [...state, newList];
         case CONSTANTS.ADD_CARD:
             const newCard = {
                 text: action.payload.text,
-                id: cardID
+                id: `card-${cardID}`
             };
             cardID += 1;
 
